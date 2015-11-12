2acfd096bf336c3cc8062087728a8e6aef2bb398
Ian Whelan <ijwhelan@gmail.com>
2015-11-12 13:35:33 -0800

    unenroll

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -32,13 +32,27 @@ module.exports = require('waterlock').actions.user({
 				if (error) {
 					return res.status(400).send(error);
 				} else {
-					return res.status(200).send("ok");
+					return res.status(200).send(results);
 				}
 			});
 		} else {
 			return res.status(400).send("Invalid request");
 		}
 	},
+
+	unenroll: function(req,res) {
+		if (req.body.user && req.body.section_id) {
+			User.unenroll(req.body.user, req.body.section_id, function(error) {
+				if (error) {
+					return res.status(400).send(error);
+				} else {
+					return res.status(200).send(results);
+				}
+			});
+		} else {
+			return res.status(400).send("Invalid request");
+		}
+	}
 
 	classes: function(req,res) {
 		if (req.query.user) {
